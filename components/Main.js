333b5d620d80903982875077eea731c1f133bd25
Mallory Antomarchi <151766818+malloai@users.noreply.github.com>
2023-12-12 19:01:41 +0100

    Added initial files

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts sur React</h1>
+            <ul className="main--facts">
+                <li>La 1ère version est sortie en 2013</li>
+                <li>Créé originalement par Kordan Walke</li>
+                <li>A plus de 100k étoiles sur Github</li>
+                <li>Maintenu par Meta, développé par Facebook, Instagram, et la communauté</li>
+                <li>A la base de milliers d'apps, y compris des applications mobiles</li>
+            </ul>
+        </main>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+        </nav>
+    )
+}
